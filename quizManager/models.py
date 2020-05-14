79eb503fcf4f364d9bc5b983fbd7c82b79e744a8
guilherme <guilherme@freshr.fr>
2020-05-14 13:56:48 +0200

    adding model + admin

--- quizManager/models.py
+++ quizManager/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    correct_response = models.CharField(max_length=50)
+    option1 = models.CharField(max_length=50)
+    option2 = models.CharField(max_length=50)
+    option3 = models.CharField(max_length=50)
